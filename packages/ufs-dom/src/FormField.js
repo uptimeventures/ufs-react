6c76a12d8b94bfc7ffeea69076788dc0812e094d
Nicholas Young <nyoung@uptime.ventures>
2018-01-03 13:29:30 -0700

    fix(docs): add license to FormField.js

--- packages/ufs-dom/src/FormField.js
+++ packages/ufs-dom/src/FormField.js
@@ -1,3 +1,9 @@
+// @flow
+
+// Copyright 2017 Uptime Ventures, Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in LICENSE.md, at the root of this repository.
+
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
